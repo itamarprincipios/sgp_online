14d715998b3bfcb6aa169886b68cc6a92d49fe45
itamarprincipios <itamarprincipios@gmail.com>
2026-01-07 14:15:55 -0400

    Update database credentials with new user and database

--- config/config.php
+++ config/config.php
@@ -10,9 +10,9 @@ return [
     ],
     'db' => [
         'host' => 'localhost',
-        'dbname' => 'u19967126l_sgpBD',
-        'username' => 'u19967126l_SGPBDADMIN',
-        'password' => 'lO5,pcgjc90Atp+GuS(4',
+        'dbname' => 'u19967126l_dbsgp',
+        'username' => 'u19967126l_dbsgpuser',
+        'password' => '8v*HaqUGQv',
         'charset' => 'utf8mb4'
     ]
 ];
