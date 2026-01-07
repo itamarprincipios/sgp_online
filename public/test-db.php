be7e75598c8ee02578abdf58d7d31c15a48f7ebb
itamarprincipios <itamarprincipios@gmail.com>
2026-01-07 15:25:57 -0400

    Add mysqli test as suggested by support

---
+++ public/test-db.php
@@ -0,0 +1,5 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn = new mysqli('localhost', 'u19967126l_dbsgpuser', 'Sgp2025Admin', 'u19967126l_dbsgp');
+echo 'Conectou com sucesso';
+?>
